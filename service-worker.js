ce6cd78d0f53b5ad6e54922daa9adae0ec63499b
kramdzan-glitch <kramdzan@gmail.com>
2026-05-28 16:45:11 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,16 @@
-const CACHE_NAME = "9amb-ippt-dashboard-v19-apps-script-api";
-
-const APP_FILES = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./config.js",
-  "./data.json",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
-];
-
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))
-    )
+    caches.keys()
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
