8adb7871fa566a748b707671af5993bbd8144970
kramdzan-glitch <kramdzan@gmail.com>
2026-05-28 12:39:49 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,9 @@
 // 9AMB IPPT SCORING DASHBOARD - Version 19 API Config
-// Paste your Google Apps Script Web App URL below after deployment.
 
 const GOOGLE_SHEET_CONFIG = {
-  enabled: false,
+  enabled: true,
 
-  // Paste your Apps Script Web App URL here.
-  apiUrl: "",
+  apiUrl: "https://script.google.com/macros/s/AKfycbwMKmJIpnO-5Bg8gAN513KGH0gEIfUne8g1VwpARR4GB19yOkuHdBCPAOJHPR3l-Wl0dw/exec",
 
-  // Fallback to data.json if the API is not ready.
   useLocalFallback: true
 };
