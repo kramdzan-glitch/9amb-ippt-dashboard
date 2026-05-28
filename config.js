444805f94b5460d9a2fae073d6ae784d28de4e58
kramdzan-glitch <kramdzan@gmail.com>
2026-05-28 16:50:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-// 9AMB IPPT SCORING DASHBOARD - Version 19 API Config
-
 const GOOGLE_SHEET_CONFIG = {
   enabled: true,
   apiUrl: "https://script.google.com/macros/s/AKfycbwMKmJIpnO-5Bg8gAN513KGH0gEIfUne8g1VwpARR4GB19yOkuHdBCPAOJHPR3l-Wl0dw/exec",
