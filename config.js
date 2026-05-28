5b1053c4e8b6e8353f044f7fabaed035fe9c3fc3
kramdzan-glitch <kramdzan@gmail.com>
2026-05-28 13:35:24 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,6 @@
 
 const GOOGLE_SHEET_CONFIG = {
   enabled: true,
-
   apiUrl: "https://script.google.com/macros/s/AKfycbwMKmJIpnO-5Bg8gAN513KGH0gEIfUne8g1VwpARR4GB19yOkuHdBCPAOJHPR3l-Wl0dw/exec",
-
-  useLocalFallback: falso
+  useLocalFallback: false
 };
